922af3f27cf3e68dccd73260e256f8b35038515e
Ayesha-te <ayeshajahangirrr@gmail.com>
2026-01-08 23:04:41 +0500

    fsd

--- core/settings.py
+++ core/settings.py
@@ -116,19 +116,20 @@ STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Media files
 USE_SUPABASE = env.bool('USE_SUPABASE', default=False)
+SUPABASE_KEY = env('SUPABASE_KEY', default='')
 
 if USE_SUPABASE:
-    # Supabase Storage (S3-compatible) configuration
-    AWS_ACCESS_KEY_ID = env('SUPABASE_S3_ACCESS_KEY_ID')
-    AWS_SECRET_ACCESS_KEY = env('SUPABASE_S3_SECRET_ACCESS_KEY')
-    AWS_STORAGE_BUCKET_NAME = env('SUPABASE_S3_BUCKET_NAME')
-    AWS_S3_ENDPOINT_URL = env('SUPABASE_S3_ENDPOINT_URL')
+    # Supabase Storage (S3-compatible) configuration using Anon Key
+    AWS_ACCESS_KEY_ID = 'supabase'
+    AWS_SECRET_ACCESS_KEY = SUPABASE_KEY
+    AWS_STORAGE_BUCKET_NAME = env('SUPABASE_S3_BUCKET_NAME', default='')
+    AWS_S3_ENDPOINT_URL = env('SUPABASE_S3_ENDPOINT_URL', default='')
     AWS_S3_REGION_NAME = env('SUPABASE_S3_REGION_NAME', default='us-east-1')
     AWS_S3_FILE_OVERWRITE = False
     AWS_DEFAULT_ACL = None
     AWS_S3_VERIFY = True
     
-    DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+    DEFAULT_FILE_STORAGE = 'core.storage.SupabaseStorage'
     MEDIA_URL = f'{AWS_S3_ENDPOINT_URL}/{AWS_STORAGE_BUCKET_NAME}/'
 else:
     MEDIA_URL = '/media/'
